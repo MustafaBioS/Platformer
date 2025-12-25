622be082efbc3bfb94816c2112ea10d931006925
MustafaBioS <mustaafahany@gmail.com>
2025-12-25 23:40:09 +0200

    ADD - Player animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Start()
@@ -18,12 +19,18 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
         body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * Speed, body.linearVelocity.y);
 
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(6, 6, 6);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-6, 6, 6);
+
         if (Input.GetKey(KeyCode.Space))
-        {
             body.linearVelocity = new Vector2(body.linearVelocity.x, JumpForce);
-        }
+
+        anim.SetBool("Run", horizontalInput != 0);
 
     }
 }
